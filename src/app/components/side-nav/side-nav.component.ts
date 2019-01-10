e735b7786c8253dc9db6a4f5a11d304bfdafc7c0
unknown <margalight@outlook.com>
2019-01-10 21:24:52 +0300

    working with responsive design

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, NavigationStart, ActivatedRoute } from '@angular/router';
-
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
+  animations: [
+    trigger('slideIn', [
+      state('hidden', style({
+      display: 'none',
+      width: '0',
+      padding: '0'
+      }))
+    ])
+  ]
 })
 export class SideNavComponent implements OnInit {
   private showBar: Boolean = false;
@@ -14,16 +23,6 @@ export class SideNavComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute
   ) {
-    // router.events
-    // // .filter((evt) => evt instanceof NavigationStart || evt instanceof NavigationEnd)
-    // .subscribe((evt) => {
-    //   console.log(evt);
-    //   if (evt instanceof NavigationStart) {
-    //     console.log('Route Change Start!');
-    //   } else if (evt instanceof NavigationEnd) {
-    //     console.log('Route Change End!');
-    //   }
-    // });
    }
 
   ngOnInit() {
